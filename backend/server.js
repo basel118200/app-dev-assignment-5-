1913333ac6a92f972b0cc670b76ae014303c735e
basel118200 <164511769+basel118200@users.noreply.github.com>
2026-04-26 08:56:49 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000']  // we'll add the Vercel URL later
+    origin: ['http://localhost:3000', 'https://app-dev-assignment-5.vercel.app']
 }));
 
 const { Pool } = require('pg');
